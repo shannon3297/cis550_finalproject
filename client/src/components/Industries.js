85dd852e5e66abd30d427b56bb1feb4f35b49e0c
Ori <ori>
2021-12-12 16:28:19 -0500

    date not working, but style done

--- client/src/components/Industries.js
+++ client/src/components/Industries.js
@@ -20,7 +20,7 @@ class Industries extends React.Component {
             stockPriceData: [],
             articles: [],
             datePicked: "2021-2-20",
-            industriesI: [{industry: 'loading', intradayMovement: 'loading...'}],
+            industriesI: [{industry: 'loading...', intradayMovement: 'loading...'}],
             industriesII: [{industry: 'loading...', numMentions: 'loading...'}],
             industriesIII: [{industry: 'loading...', mentionIncrease: 'loading...', avgRange: 'loading...'}],
             industriesIV: [{industry: 'loading...', performance: 'loading...'}]
@@ -35,7 +35,7 @@ class Industries extends React.Component {
     // }
 
     componentDidMount() {
-        fetch(SERVER_URL + "/industriesMostVolatility?date=" + this.state.datePicked) // industriesI
+        fetch(SERVER_URL + "/industriesMostVolatility") // industriesI ?date=" + this.state.datePicked
             .then((res) => res.json())
             .then((result) => {
                 this.setState({ industriesI: result.results })
@@ -61,17 +61,60 @@ class Industries extends React.Component {
     }
     
     render() {
+        const sectionHeader = {
+            // paddingTop: '120px',
+            fontSize: '40px',
+            fontStyle: 'normal',
+            fontWeight: '700',
+            lineHeight: '48px',
+            letterSpacing: '0.20000000298023224px',
+            textAlign: 'center'
+        };
+
+        const subSection = {
+        };
+
+        const Heading = {
+            fontSize: '20px',
+
+            paddingTop: '100px',
+            fontWeight: "bold"
+        };
+
+        const subHeading = {
+            width: "400px",
+            paddingRight: '50px',
+            paddingBottom: '30px',
+            textAlign: 'justify'
+
+        };
+
+        const columnHeader = {
+            paddingTop: '100px',
+
+            width: "200px",
+            fontWeight: "bold",
+            padding: '10px'
+        };
+
+        const content = {
+            padding: '10px'
+        };
+
+
+      
         return (
             <>  
-                <div className="bg-white w-full flex-center p-8">           {/* Section I */}
-                    <div className="text-lg my-4">I. intraday volatility</div>
-                    <div className="text-sm my-4">This measure uses the highest and lowest  prices of the day.</div>
-                    <div className="container flex-center"> {/*Row #1: Some of the simple stats */}
+                
+                <div className="bg-white w-full flex-center p-8 paddingBottom p-60">           {/* Section I */}
+                    <div style={sectionHeader} >I. intraday volatility</div>
+                    <div style={subSection} className="text-sm my-4">This measure uses the highest and lowest  prices of the day.</div>
+                    <div className="container flex-center"> 
                         <div className="flex flex-apart text-black">
                             <div>
                                 
-                                <div className="text-lg my-4">Top @ [Enter Date]</div>
-                                <div className="text-sm my-4">Identify industries that saw the most intraday volatility on a given date.</div>
+                                <div style={Heading} className="text-lg my-4">Top @ [Enter Date]</div>
+                                <div style={subHeading} className="text-sm my-4">Identify industries that saw the most intraday volatility on a given date.</div>
                             
                                 <LocalizationProvider dateAdapter={AdapterDateFns}>
                                     <DesktopDatePicker
@@ -93,85 +136,85 @@ class Industries extends React.Component {
                             </div>
                             <div className="flex flex-row">
                                 <div>
-                                    <div>Industry</div>
-                                    {this.state.industriesI.map((v) => {return( <h6>{v.industry} </h6>)})}
+                                    <div style={columnHeader} >Industry</div>
+                                    {this.state.industriesI.map((v) => {return( <h6 style={content}>{v.industry} </h6>)})}
                                 </div>
                                 <div>
-                                    <div>Movement</div>
-                                    {this.state.industriesI.map((v) => {return( <h6>{v.intradayMovement} </h6>)} )}
+                                    <div style={columnHeader} >Movement</div>
+                                    {this.state.industriesI.map((v) => {return( <h6 style={content}>{v.intradayMovement} </h6>)} )}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="bg-blue text-white w-full flex-center p-8"> {/* Section II */}
-                    <div className="text-lg my-4">II. the famous ones</div>
-                    <div className="text-sm my-4">Over the last.</div>
+                <div className="bg-blue text-white w-full flex-center p-8 paddingBottom p-60"> {/* Section II */}
+                    <div style={sectionHeader} className="text-lg my-4">II. the famous ones</div>
+                    <div className="text-sm my-4">Over the last 30 days, some industries were popular!</div>
                     <div className="container flex-center">
                         <div className="flex flex-apart text-white">
                             
                             
                             <div>
-                                <div className="text-lg my-4">Industries in the news</div>
-                                <div className="text-sm my-4">Here’s the industries receiving the top 3 number of company mentions in WSJ articles. </div>
+                                <div style={Heading} className="text-lg my-4">Industries in the news</div>
+                                <div style={subHeading} className="text-sm my-4">Here’s the industries receiving the top 3 number of company mentions in WSJ articles. </div>
                             </div>
                             <div className="flex flex-row">
                                 <div>
-                                    <div>Industry</div>
-                                    {this.state.industriesII.map((v) => {return( <h6>{v.industry} </h6>)} )}
+                                    <div style={columnHeader} >Industry</div>
+                                    {this.state.industriesII.map((v) => {return( <h6 style={content}>{v.industry} </h6>)} )}
                                 </div>
                                 <div>
-                                    <div>News Mentions</div>
-                                    {this.state.industriesII.map((v) => {return( <h6>{v.numMentions} </h6>)} )}
+                                    <div style={columnHeader} >News Mentions</div>
+                                    {this.state.industriesII.map((v) => {return( <h6 style={content}>{v.numMentions} </h6>)} )}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="bg-white w-full flex-center p-8">           {/* Section III */}
-                    <div className="text-lg my-4">III. soon risers</div>
+                <div className="bg-white w-full flex-center p-8 paddingBottom p-60">           {/* Section III */}
+                    <div style={sectionHeader} className="text-lg my-4">III. soon risers</div>
                     <div className="text-sm my-4"></div>
                     <div className="container flex-center">
                         <div className="flex flex-apart text-black">
                             <div>
-                                <div className="text-lg my-4">Slow now, but starting to buzz</div>
-                                <div className="text-sm my-4">Based on increased mentions of these stocks in WSJ, we expect them to start moving - either up or down. Soon!
+                                <div style={Heading} className="text-lg my-4">Slow now, but starting to buzz</div>
+                                <div style={subHeading} className="text-sm my-4">Based on increased mentions of these stocks in WSJ, we expect them to start moving - either up or down. Soon!
                                                               Specifically, if the stock has seen +50% increase in press coverage over the last 30 days, compared to the last 60 days, and has remained fairly price-constant, then we expect it to move soon.</div>
                             </div>
                             <div className="flex flex-row">
                                 <div>
-                                    <div>Industry</div>
-                                    {this.state.industriesIII.map((v) => {return( <h6>{v.industry} </h6>)} )}
+                                    <div style={columnHeader} >Industry</div>
+                                    {this.state.industriesIII.map((v) => {return( <h6 style={content}>{v.industry} </h6>)} )}
                                 </div>
                                 <div>
-                                    <div>Increase in mentions</div>
-                                    {this.state.industriesIII.map((v) => {return( <h6>{v.mentionIncrease} </h6>)} )}
+                                    <div style={columnHeader} >Increase in mentions</div>
+                                    {this.state.industriesIII.map((v) => {return( <h6 style={content}>{v.mentionIncrease} </h6>)} )}
                                 </div>
                                 <div>
-                                    <div>Price Range</div>
-                                    {this.state.industriesIII.map((v) => {return( <h6>{v.avgRange} </h6>)} )}
+                                    <div style={columnHeader} >Price Range</div>
+                                    {this.state.industriesIII.map((v) => {return( <h6 style={content}>{v.avgRange} </h6>)} )}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="bg-blue text-white w-full flex-center p-8"> {/* Section IV */}
-                    <div className="text-lg my-4">IV. yesterday is yesterday</div>
+                <div className="bg-blue text-white w-full flex-center p-8 padding p-60"> {/* Section IV */}
+                    <div style={sectionHeader} className="text-lg my-4">IV. yesterday is yesterday</div>
                     <div className="text-sm my-4"></div>
                     <div className="container flex-center">
                         <div className="flex flex-apart text-white">
                             <div>
-                                <div className="text-lg my-4">Industry performance yesterday</div>
-                                <div className="text-sm my-4">Based on yesterday’s performance of companies in the particular industries, we sort them based on best to worst performance.</div>
+                                <div style={Heading} className="text-lg my-4">Industry performance yesterday</div>
+                                <div style={subHeading} className="text-sm my-4">Based on yesterday’s performance of companies in the particular industries, we sort them based on best to worst performance.</div>
                             </div>
                             <div className="flex flex-row">
                                 <div>
-                                    <div>Industry</div>
-                                    {this.state.industriesIV.map((v) => {return( <h6>{v.industry} </h6>)} )}
+                                    <div style={columnHeader} >Industry</div>
+                                    {this.state.industriesIV.map((v) => {return( <h6 style={content}>{v.industry} </h6>)} )}
                                 </div>
                                 <div>
-                                    <div>Performance</div>
-                                    {this.state.industriesIV.map((v) => {return( <h6>{v.performance} </h6>)} )}
+                                    <div style={columnHeader} >Performance</div>
+                                    {this.state.industriesIV.map((v) => {return( <h6 style={content}>{v.performance} </h6>)} )}
                                 </div>
                             </div>
                         </div>
